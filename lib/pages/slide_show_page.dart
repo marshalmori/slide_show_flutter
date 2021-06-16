7c6f8b1cfcd84b97024c2a998cad0fc779051f95
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-06-16 09:24:39 -0300

    Create assets directory and show SVGs

--- lib/pages/slide_show_page.dart
+++ lib/pages/slide_show_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Olá Devs'),
+        child: SvgPicture.asset('assets/svgs/1.svg'),
       ),
     );
   }
