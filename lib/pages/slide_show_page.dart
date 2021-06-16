ef40b717d35d5c4f0ffdc1d81ebff0e39650c863
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-06-16 09:37:49 -0300

    Create PageView andcreate _Slide class

--- lib/pages/slide_show_page.dart
+++ lib/pages/slide_show_page.dart
@@ -6,8 +6,41 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/1.svg'),
+        child: _Slides(),
       ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide('assets/svgs/1.svg'),
+          _Slide('assets/svgs/2.svg'),
+          _Slide('assets/svgs/3.svg'),
+          _Slide('assets/svgs/4.svg'),
+          _Slide('assets/svgs/5.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: SvgPicture.asset(svg),
     );
   }
 }
