1abbc914f46d1f81a7124d955dbe7779168c1c61
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-06-21 22:10:07 -0300

    Refactoring SlideshowPage

--- lib/pages/slideshow_page.dart
+++ lib/pages/slideshow_page.dart
@@ -6,20 +6,27 @@ class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Slideshow(
-        primaryDotSize: 12,
-        secundaryDotSize: 12,
-        pointsAbove: false,
-        primaryColorDots: Colors.purple.shade900,
-        secundaryColorDots: Colors.grey,
-        slides: [
-          SvgPicture.asset('assets/svgs/1.svg'),
-          SvgPicture.asset('assets/svgs/2.svg'),
-          SvgPicture.asset('assets/svgs/3.svg'),
-          SvgPicture.asset('assets/svgs/4.svg'),
-          SvgPicture.asset('assets/svgs/5.svg'),
-        ],
-      ),
+      body: SingleSlidshow(),
+    );
+  }
+}
+
+class SingleSlidshow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      primaryDotSize: 12,
+      secundaryDotSize: 12,
+      pointsAbove: false,
+      primaryColorDots: Colors.purple.shade900,
+      secundaryColorDots: Colors.grey,
+      slides: [
+        SvgPicture.asset('assets/svgs/1.svg'),
+        SvgPicture.asset('assets/svgs/2.svg'),
+        SvgPicture.asset('assets/svgs/3.svg'),
+        SvgPicture.asset('assets/svgs/4.svg'),
+        SvgPicture.asset('assets/svgs/5.svg'),
+      ],
     );
   }
 }
